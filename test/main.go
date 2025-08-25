bd1b10863f4e806fd8006025e3845f8cafa243e6
root <gitlab_admin_2925d1@example.com>
2025-08-25 03:22:55 -0400

    update dao

---
+++ test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"HiChat/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:Welcome12@tcp(127.0.0.1:3306)/HiChat?charset=utf8mb4&parseTime=True&loc=Local"
+	db,err:=gorm.Open(mysql.Open(dsn),&gorm.Config{})
+	if err!=nil{
+		panic(err)
+	}
+	err=db.AutoMigrate(&models.UserBasic{})
+	if err!=nil{
+		panic(err)
+	}
+}	
